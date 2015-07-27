aecf9d6d11c0a569914c26d736a9abde132ca636
asfink <ali@finkelstein.us>
2015-07-27 16:58:58 -0400

    populating methods for the seedb_backend interface

--- backendv2/postgres/src/wrapperInterface/SeeDB_Backend.java
+++ backendv2/postgres/src/wrapperInterface/SeeDB_Backend.java
@@ -5,4 +5,32 @@ package wrapperInterface;
  */
 public interface SeeDB_Backend {
 
+	/*
+	 * Establishes connection to a database.
+	 * 
+	 * @param database name of the database to connect to
+	 * 
+	 * @param address ip address/weblocation of the database to connect to
+	 * 
+	 * @param username username for database login information
+	 * 
+	 * @param password password for database login information
+	 * 
+	 * @author Ali Finkelstein
+	 * @date 27 July 2015
+	 */
+	public void connectToDB(String databaseName, String address,
+			String username, String password);
+
+	/*
+	 * Populates a table in the Database to use for calculations and searches
+	 * for the SeeDB project. Either it creates a new table, if possible, or
+	 * uses table that already exists.
+	 * 
+	 * @param databaseName name of the database to connect to
+	 * 
+	 * @author Ali Finkelstein
+	 * @date 27 July 2015
+	 */
+	public void populateTableInfoForDB(String databaseName);
 }
